ab3e881185e8196ff9ff2dd3e0516b58285d9770
David H <91997735+Noweyr@users.noreply.github.com>
2022-01-26 13:42:32 -0600

    Fixed file location and major overhaul - See extended description.
    Greatly improved style and position of elements. Main game calculations simplified from multi-dimensional array to single-dimension array. Added animated "Play!" button and rules explaining how to play. Animated elements that hide and reveal game window.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,17 @@
-/*
-RULES
-1) user inputs which cells initially alive or dead with mouseclick ***DONE***
-2) Any live cell with two or three live neighbours survives.
-3) Any dead cell with three live neighbours becomes a live cell.
-4) All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-*/
-
 // Sets number of rows and columns
 let rows = 200;
-let cols = 200;
+let cols = 300;
 let initialValues = [];
-let rowValues = [];
-let nextGridValues = [];
 let tick = 0;
-let tempValues = [...Array(rows)].map(e => Array(cols).fill(0));
+let tempValues;
+let zerosArray = [...Array(rows * cols)].fill(0);
 
 // Creates grid
 for (let i = 0; i < rows; i++) {
     let newDivContainer = document.createElement('div');
     newDivContainer.id = `row${i}`;
     newDivContainer.className = 'grid';
-    document.getElementById('main').appendChild(newDivContainer);
+    document.getElementById('game-window').appendChild(newDivContainer);
     for (let j = 0; j < cols; j++) {
         let newDiv = document.createElement('div');
         newDiv.id = `x${j}y${i}`;
@@ -33,8 +24,6 @@ for (let i = 0; i < rows; i++) {
 // Creates array like object 'cell' and array of selectors from 'cell'
 let cell = document.getElementsByClassName('cell');
 let selectorArray = Array.from(cell);
-let copySelectorArray = selectorArray;
-let selector2DArray = [];
 
 // Allows setting of initial cell conditions
 let switchColor = (event) => {
@@ -54,45 +43,63 @@ let resetCells = () => {
 
 document.getElementById('reset').addEventListener('click', resetCells);
 
-// Creates a 2D array of HTML selectors for each cell
-while (copySelectorArray.length) {
-    selector2DArray.push(copySelectorArray.splice(0,cols));
-}
-
 // Sets border cell visibilty to hidden
 // Top row
 for (let i = 0; i < cols; i++) {
-    selector2DArray[0][i].style.visibility = 'hidden';
+    selectorArray[i].style.visibility = 'hidden';
 }
 // Bottom row
-for (let i = 0; i < cols; i++) {
-    selector2DArray[rows - 1][i].style.visibility = 'hidden';
+for (let i = (cols * (rows - 1)); i < (rows * cols); i++) {
+    selectorArray[i].style.visibility = 'hidden';
 }
 // Left side
-for (let i = 0; i < rows; i++) {
-    selector2DArray[i][0].style.visibility = 'hidden';
+for (let i = cols; i <= (cols * (rows - 2)); i += cols) {
+    selectorArray[i].style.visibility = 'hidden';
 }
 // Right side
-for (let i = 0; i < rows; i++) {
-    selector2DArray[i][rows - 1].style.visibility = 'hidden';
+for (let i = (cols * 2) - 1; i < (cols * (rows - 1)); i += cols) {
+    selectorArray[i].style.visibility = 'hidden';
 }
+
+// Sleep function for delays in transition
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Transition from intro to main game
+const transition = async () => {
+    let introElements = Array.from(document.getElementsByClassName('intro'));
+    let rulesList = Array.from(document.getElementsByClassName('rules-list'));
+
+    await sleep(700);
+    introElements.forEach(e => e.style.transition = 'transform .6s ease-in-out');
+    introElements.forEach(e => e.style.transform = 'scale(0)');
+    rulesList.forEach(e => e.style.transition = 'transform .6s ease-in-out');
+    rulesList.forEach(e => e.style.transform = 'scale(.58, .65) translate(-63rem, 0)');
+
+    await sleep(100);
+    let gameButtons = document.getElementById('button-container');
+    gameButtons.style.visibility = 'visible';
+    gameButtons.style.transition = 'opacity .6s ease-in-out';
+    gameButtons.style.opacity = '1';
+    
+    let gameWindow = document.getElementById('game-window');
+    gameWindow.style.visibility = 'visible';
+    gameWindow.style.transition = 'opacity .6s ease-in-out';
+    gameWindow.style.opacity = '1';
+}
+
+document.getElementById('play').addEventListener('click', transition);
 
 // Creates array of cell status values (0 or 1) based on current grid colors
 let initialGridValues = () => {
-    initialValues.length = 0;
 
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            if ((selector2DArray[i][j]).style.backgroundColor === 'gray') {
-                rowValues.push(0);
-            } else if ((selector2DArray[i][j]).style.backgroundColor === 'lightgreen') {
-                rowValues.push(1);
-            }
+    for (let i = 0; i < cell.length; i++) {
+        if (selectorArray[i].style.backgroundColor === 'gray') {
+            initialValues.push(0);
+        } else if (selectorArray[i].style.backgroundColor === 'lightgreen') {
+            initialValues.push(1);
         }
-    }
-
-    while(rowValues.length) {
-        initialValues.push(rowValues.splice(0, cols));
     }
 
     document.getElementById('step').removeEventListener('click', initialGridValues);
@@ -103,43 +110,44 @@ let initialGridValues = () => {
 // Calculates grid values for the next "tick"
 let calculateNextGridValues = () => {
     let sum;
-    let nextValues = [...Array(rows)].map(e => Array(cols).fill(0));
-    let currentValues = [...Array(rows)].map(e => Array(cols).fill(0));
+    let nextValues = zerosArray.slice();
+    let currentValues;
 
     if (tick === 0) {
-        for (let i = 0; i < initialValues.length; i++) {
-            currentValues[i] = initialValues[i].slice();
-        }  
+        currentValues = initialValues.slice(); 
     } else {
-        for (let i = 0; i < tempValues.length; i++) {
-            currentValues[i] = tempValues[i].slice();
-        }
+        currentValues = tempValues.slice();
     }
 
-    // Calculate all cells excluding border cells
-    for (let i = 1; i < rows - 1; i++) {
-        for (let j = 1; j < cols - 1; j++) {
-        sum = currentValues[j][i-1] + currentValues[j+1][i-1] + currentValues[j+1][i] + currentValues[j+1][i+1] + currentValues[j][i+1] + currentValues[j-1][i+1] + currentValues[j-1][i] + currentValues[j-1][i-1];
-        if (currentValues[j][i] === 0 && sum === 3) {nextValues[j].splice(i,1,1);}
-        else if (currentValues[j][i] === 1 && (sum === 2 || sum === 3)) {nextValues[j].splice(i,1,1);}    
-        }
-    }
-    
-    // Recolors grid
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++) {
-            if (nextValues[j][i] === 0) {
-                selector2DArray[j][i].style.backgroundColor = 'gray';
-            } else if (nextValues[j][i] === 1) {
-                selector2DArray[j][i].style.backgroundColor = 'lightgreen';
+    // Calculate whether cell is alive or dead
+    for (let i = cols + 1; i < ((rows - 1) * cols); i++) {
+        if (i % cols === 0) {
+            currentValues.splice(i, 1, 0);
+            currentValues.splice(i - 1, 1, 0);
+        } else {
+            sum = currentValues[(i-1)-cols] + currentValues[i-cols] + currentValues[(i+1)-cols] + currentValues[i-1] + currentValues[i+1] + currentValues[(i-1)+cols] + currentValues[i+cols] + currentValues[(i+1)+cols];          
+            if (currentValues[i] === 0 && sum === 3) {
+                nextValues.splice(i,1,1);
+            } else if (currentValues[i] === 1 && (sum === 2 || sum === 3)) {
+                nextValues.splice(i,1,1);
             }
         }
     }
-    tick++;
-    tempValues = [...Array(rows)].map(e => Array(cols).fill(0));
-    for (let i = 0; i < nextValues.length; i++) {
-        tempValues[i] = nextValues[i].slice();
+
+    // Recolors grid
+    for (let i = cols + 1; i < ((rows - 1) * cols); i++) {
+        if (i % cols === 0) {
+            continue;
+        } else if (i % cols === cols - 1) {
+            continue;
+        } else if (nextValues[i] === 0) {
+            selectorArray[i].style.backgroundColor = 'gray';
+        } else if (nextValues[i] === 1) {
+            selectorArray[i].style.backgroundColor = 'lightgreen';
+        }
     }
+    tick++
+    tempValues = nextValues.slice();
     return tempValues;
 }
 
@@ -162,15 +170,19 @@ document.getElementById('run').addEventListener('click', run);
 
 // Randomize button functionality
 let randomize = () => {
-    tempValues = [...Array(rows)].map(e => Array(cols).fill(0));
+    tempValues = [...Array(rows * cols)].fill(0);
 
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++) {
-            tempValues[j][i] = Math.round(Math.random());
-            if (tempValues[j][i] === 0) {
-                selector2DArray[j][i].style.backgroundColor = 'gray';
-            } else if (tempValues[j][i] === 1) {
-                selector2DArray[j][i].style.backgroundColor = 'lightgreen';
+    for (let i = cols + 1; i < ((rows - 1) * cols); i++) {
+        if (i % cols === 0) {
+            continue;
+        } else if (i % cols === cols - 1) {
+            continue;
+        } else {
+            tempValues[i] = Math.round(Math.random());
+            if (tempValues[i] === 0) {
+                selectorArray[i].style.backgroundColor = 'gray';
+            } else if (tempValues[i] === 1) {
+                selectorArray[i].style.backgroundColor = 'lightgreen';
             }
         }
     }
